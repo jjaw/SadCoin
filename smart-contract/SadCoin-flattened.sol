2358fe6405c93e10b207cb45a499d0a2ea733bc1
will <will.jaw@gmail.com>
2023-06-14 02:43:59 -0700

    Update SadCoin-flattened.sol
    Changed to Sepolia testnet

--- smart-contract/SadCoin-flattened.sol
+++ smart-contract/SadCoin-flattened.sol
@@ -637,7 +637,7 @@ contract SadCoin is ERC20, Ownable {
     uint256 public maxSupply; // Maximum supply of tokens
 
     constructor() ERC20("SadCoin", "SAD") {
-        priceFeed = AggregatorV3Interface(0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e); // ETH/USD Price Feed on Goerli
+        priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306); // ETH/USD Price Feed on Sepolia
         maxSupply = 1000000000; // Set maximum supply to 1 billion tokens
         _mint(msg.sender, maxSupply / 2); // Mint half of the tokens to the contract creator
     }
@@ -649,7 +649,7 @@ contract SadCoin is ERC20, Ownable {
     function claimToken() public payable {
         require(totalSupply() + 1 <= maxSupply, "Max supply reached"); // Check if max supply would be exceeded
         int ethPrice = getLatestPrice();
-        require(msg.value >= 1 ether * 10**8 / uint256(ethPrice), "Not enough ETH sent!"); // Require $1 worth of ETH
+        require(msg.value >= 1 ether * 10**8 / uint256(ethPrice), "Not enough ETH sent"); // Require $1 worth of ETH
         _mint(msg.sender, 1); // Mint 1 token to the sender
     }
 
